44eee94bd9d83e4f7f2e05e68b185b3512babdec
omnikorn <harith@Hariths-MacBook-Air-2.local>
2023-06-15 22:58:06 +0100

    track render added

--- src/components/playlist/playlist.js
+++ src/components/playlist/playlist.js
@@ -8,7 +8,7 @@ class Playlist extends Component {
         return (
             <div className="Playlist">
                 <input value={'New Playlist'} />
-                <TrackList />
+               
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
